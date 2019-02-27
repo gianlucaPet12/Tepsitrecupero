7c8ff954db12d1cbe763e5c7b5a34f5cc7c79abe
studenti <studenti@rosselli.loc>
2019-02-27 15:34:30 +0100

    Add project files.

---
+++ Binary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            Console.WriteLine(a);
+            Console.WriteLine(a + 3);
+
+            Console.WriteLine();
+
+            int b = int.MinValue;
+            Console.WriteLine(b);
+            //Console.WriteLine(b + 1);
+
+
+
+            Console.Read();
+        }
+    }
+}
